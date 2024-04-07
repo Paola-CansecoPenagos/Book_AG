9040abfce54b504faa19c9f0a9d6476904c63cde
Paola-CansecoPenagos <paolaacp16@gmail.com>
2024-04-07 00:49:03 -0600

    feat: Adding scripting tool

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("Hello World")
+import json
+
+datos_limpios = []
+ides = []
+
+with open("dataset/sampledata.json", "r",  encoding="utf8") as f:
+    datos = json.load(f)
+
+for registro in datos:
+    if (registro["id"] in ides):
+        print("Saltando por repetición.")
+        break
+
+    ides.append(registro["id"])
+
+    if "authors" in registro.get("volumeInfo", {}):
+        autores = registro["volumeInfo"]["authors"]
+    else:
+        autores = ["Anónimo"]
+
+    if "publishedDate" in registro.get("volumeInfo", {}):
+        fecha_de_registro = registro["volumeInfo"]["publishedDate"]
+    else:
+        fecha_de_registro = "Sin información."
+
+    if "imageLinks" in registro.get("volumeInfo", {}):
+        imagen_uri = registro["volumeInfo"]["imageLinks"]["thumbnail"]
+    else:
+        imagen_uri = "#"
+
+    if "categories" in registro.get("volumeInfo", {}):
+        categorie = registro["volumeInfo"]["categories"]
+    else:
+        break  # No nos importa libros sin categoría.
+
+    datos_limpios.append({
+        "title": registro["volumeInfo"]["title"],
+        "autores": autores,
+        "fecha": fecha_de_registro,
+        "imagen": imagen_uri,
+        "categorias": categorie
+    })
+
+with open('dataset/cleareddata.json', 'w', encoding='utf-8') as archivo_salida:
+    json.dump(datos_limpios, archivo_salida, indent=4, ensure_ascii=False)
+
+print("Datos limpios guardados en cleareddata.json.")
---
+++ tools/cleardata.mjs
@@ -0,0 +1,55 @@
+/**
+ * Este script para limpiar y depurar datos de las colecciones en crudo del API
+ */
+
+import datas from '../dataset/sampledata.json' assert {type:'json'};
+import fs from 'fs';
+
+let id = 0;
+
+const datosLimpios = [];
+const ides = new Set();
+const lista_categorias = new Set();
+const lista_autores = new Set();
+
+datas.map((data, index) => {
+    if (ides.has(data.id)) {
+        console.log("Saltando por repetición.");
+        return;
+    }
+
+    ides.add(data.id);
+
+    const autores = data.volumeInfo.authors || ["Anónimo"];
+    const fechaDeRegistro = data.volumeInfo.publishedDate || "Sin información.";
+    const imagenUri = (data.volumeInfo.imageLinks && data.volumeInfo.imageLinks.thumbnail) || "#";
+    const categorias = data.volumeInfo.categories;
+
+    if (!categorias) {
+        console.log("No nos importa libros sin categoría.");
+        return;
+    }
+
+    for (const categoria of categorias) {
+        lista_categorias.add(categoria);
+    }
+
+    for (const autor of autores) {
+        lista_autores.add(autor);
+    }
+
+    datosLimpios.push({
+        id: ++id,
+        title: data.volumeInfo.title,
+        autores: autores,
+        fecha: fechaDeRegistro,
+        imagen: imagenUri,
+        categorias: categorias
+    });
+});
+
+fs.writeFileSync('../dataset/cleareddata.json', JSON.stringify(datosLimpios, null, 4), 'utf8');
+fs.writeFileSync('../dataset/categories.json', JSON.stringify([...lista_categorias], null, 4), 'utf8');
+fs.writeFileSync('../dataset/authors.json', JSON.stringify([...lista_autores], null, 4), 'utf8');
+
+console.log("Datos limpios guardados en cleareddata.json.");
